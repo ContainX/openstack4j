876abccb566fca49a8ce14f47b92f5fc2da488de
Jeremy Unruh <jeremyunruh@sbcglobal.net>
2014-04-01 22:53:14 -0700

    Block Storage (Cinder) volumes

--- src/main/java/org/openstack4j/openstack/storage/block/internal/BlockVolumeServiceImpl.java
+++ src/main/java/org/openstack4j/openstack/storage/block/internal/BlockVolumeServiceImpl.java
@@ -1,9 +1,14 @@
 package org.openstack4j.openstack.storage.block.internal;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.List;
 
 import org.openstack4j.api.storage.BlockVolumeService;
+import org.openstack4j.model.storage.block.Volume;
 import org.openstack4j.model.storage.block.VolumeType;
+import org.openstack4j.openstack.storage.block.domain.CinderVolume;
+import org.openstack4j.openstack.storage.block.domain.CinderVolume.Volumes;
 import org.openstack4j.openstack.storage.block.domain.CinderVolumeType.VolumeTypes;
 
 /**
@@ -19,6 +24,23 @@ public class BlockVolumeServiceImpl extends BaseBlockStorageServices implements 
 	@Override
 	public List<? extends VolumeType> listVolumeTypes() {
 		return get(VolumeTypes.class, uri("/types")).execute().getList();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<? extends Volume> list() {
+		return get(Volumes.class, uri("/volumes")).execute().getList();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Volume get(String volumeId) {
+		checkNotNull(volumeId);
+		return get(CinderVolume.class, uri("/volumes/%s", volumeId)).execute();
 	}
 
 }
