582e60b42e23dc94e61cac009f9906265164682e
n-r-anderson <nr_anderson@comcast.net>
2014-04-07 16:27:10 -0700

    added SecurityGroup, SecurityGroupRule, NetFloatingIP

---
+++ src/main/java/org/openstack4j/api/networking/SecurityGroupService.java
@@ -0,0 +1,48 @@
+package org.openstack4j.api.networking;
+
+import java.util.List;
+
+import org.openstack4j.common.RestService;
+import org.openstack4j.model.network.SecurityGroup;
+
+
+/**
+ * Provides Neutron-based Security Group services.
+ *
+ * @author Nathan Anderson
+ */
+public interface SecurityGroupService extends RestService {
+
+  
+  /**
+   * Get list of security groups accessible by the current tenant
+   *
+   * @return the list<? extends security group>
+   */
+  List<? extends SecurityGroup> list();
+  
+  
+  /**
+   * Gets the Security Group by id.
+   *
+   * @param id the id
+   * @return the security group
+   */
+  SecurityGroup get(String id);
+  
+  /**
+   * Deletes SecurityGroup by id.
+   *
+   * @param id SecurityGroup id
+   */
+  void delete(String id);
+  
+  /**
+   * Creates a SecurityGroup.
+   *
+   * @param securityGroup the security group
+   * @return the security group
+   */
+  SecurityGroup create(SecurityGroup securityGroup);
+  
+}
