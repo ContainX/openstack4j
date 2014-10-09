01cbdffc7280f40ba617b3ce539652c348d0f732
Jeremy Unruh <jeremyunruh@sbcglobal.net>
2014-10-09 15:35:15 -0700

    add Logging abstraction layer to offer logger extensions allowing users to decide which Logging impl they would like OS4J to use

---
+++ connectors/resteasy/src/main/java/org/openstack4j/connectors/resteasy/executors/ApacheHttpClientExecutor.java
@@ -0,0 +1,36 @@
+package org.openstack4j.connectors.resteasy.executors;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.jboss.resteasy.client.core.executors.ApacheHttpClient4Executor;
+import org.openstack4j.core.transport.Config;
+
+/**
+ * Default Apache HttpClient based Executor
+ * 
+ * @author Jeremy Unruh
+ */
+public class ApacheHttpClientExecutor extends ApacheHttpClient4Executor {
+
+    ApacheHttpClientExecutor(HttpClient client) {
+        super(client);
+    }
+    
+    public static ApacheHttpClientExecutor create(Config config) {
+        
+        HttpParams params = new BasicHttpParams();
+        if (config.getReadTimeout() > 0)
+            HttpConnectionParams.setSoTimeout(params, config.getReadTimeout());
+        
+        if (config.getConnectTimeout() > 0)
+            HttpConnectionParams.setConnectionTimeout(params, config.getConnectTimeout());
+
+        HttpClient client = new DefaultHttpClient(params);
+        
+        return new ApacheHttpClientExecutor(client);
+    }
+    
+}
---
+++ core/src/main/java/org/openstack4j/openstack/logging/internal/FallbackLoggerFactorySupplier.java
@@ -0,0 +1,34 @@
+package org.openstack4j.openstack.logging.internal;
+
+import org.openstack4j.openstack.logging.Logger;
+import org.openstack4j.openstack.logging.Logger.LoggerFactorySupplier;
+
+/**
+ * Fallback Logger Factory Supplier which returns a Console logger by default only logging WARN and ERROR levels
+ * 
+ * @author Jeremy Unruh
+ *
+ */
+public final class FallbackLoggerFactorySupplier implements LoggerFactorySupplier {
+
+    private static final FallbackLoggerFactorySupplier INSTANCE = new FallbackLoggerFactorySupplier();
+    
+    private FallbackLoggerFactorySupplier() { 
+    }
+    
+    public static FallbackLoggerFactorySupplier getInstance() {
+        return INSTANCE;
+    }
+    
+    
+    @Override
+    public Logger getLogger(String category) {
+        return new ConsoleLogger(category);
+    }
+
+    @Override
+    public Logger getLogger(Class<?> category) {
+        return getLogger(category.getName());
+    }
+
+}
