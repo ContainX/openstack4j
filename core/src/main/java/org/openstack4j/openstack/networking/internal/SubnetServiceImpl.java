78fe4816dc19c25f3fffbd503d2ce434d2ffc874
Jeremy Unruh <jeremyunruh@sbcglobal.net>
2014-12-17 08:50:41 -0800

    add extra update method in subnet service

--- core/src/main/java/org/openstack4j/openstack/networking/internal/SubnetServiceImpl.java
+++ core/src/main/java/org/openstack4j/openstack/networking/internal/SubnetServiceImpl.java
@@ -18,42 +18,48 @@ import org.openstack4j.openstack.networking.domain.NeutronSubnetUpdate;
  */
 public class SubnetServiceImpl extends BaseNetworkingServices implements SubnetService {
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public List<? extends Subnet> list() {
-		return get(Subnets.class, uri("/subnets")).execute().getList();
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<? extends Subnet> list() {
+        return get(Subnets.class, uri("/subnets")).execute().getList();
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Subnet get(String subnetId) {
-		checkNotNull(subnetId);
-		return get(NeutronSubnet.class, uri("/subnets/%s", subnetId)).execute();
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Subnet get(String subnetId) {
+        checkNotNull(subnetId);
+        return get(NeutronSubnet.class, uri("/subnets/%s", subnetId)).execute();
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public ActionResponse delete(String subnetId) {
-		checkNotNull(subnetId);
-		return deleteWithResponse(uri("/subnets/%s", subnetId)).execute();
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ActionResponse delete(String subnetId) {
+        checkNotNull(subnetId);
+        return deleteWithResponse(uri("/subnets/%s", subnetId)).execute();
+    }
 
-	@Override
-	public Subnet create(Subnet subnet) {
-		checkNotNull(subnet);
-		return post(NeutronSubnet.class, uri("/subnets")).entity(subnet).execute();
-	}
-	
-	public Subnet update(Subnet subnet) {
-	  checkNotNull(subnet);
-	  return put(NeutronSubnet.class, uri("/subnets/%s", subnet.getId()))
-	            .entity(NeutronSubnetUpdate.createFromSubnet(subnet))
-	            .execute();
-	}
+    @Override
+    public Subnet create(Subnet subnet) {
+        checkNotNull(subnet);
+        return post(NeutronSubnet.class, uri("/subnets")).entity(subnet).execute();
+    }
+
+    public Subnet update(Subnet subnet) {
+        checkNotNull(subnet);
+        return update(subnet.getId(), subnet);
+    }
+
+    public Subnet update(String subnetId, Subnet subnet) {
+        checkNotNull(subnetId);
+        checkNotNull(subnet);
+        return put(NeutronSubnet.class, uri("/subnets/%s", subnetId))
+                .entity(NeutronSubnetUpdate.createFromSubnet(subnet))
+                .execute();
+    }
 }
