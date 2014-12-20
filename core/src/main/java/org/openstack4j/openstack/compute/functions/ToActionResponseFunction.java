829fce0dc8a1ed0dee0cee410c8064e2b88950ca
Jeremy Unruh <jeremyunruh@sbcglobal.net>
2014-12-19 23:10:53 -0800

    constants

--- core/src/main/java/org/openstack4j/openstack/compute/functions/ToActionResponseFunction.java
+++ core/src/main/java/org/openstack4j/openstack/compute/functions/ToActionResponseFunction.java
@@ -16,7 +16,9 @@ public class ToActionResponseFunction implements Function<HttpResponse, ActionRe
 
     public static final ToActionResponseFunction INSTANCE = new ToActionResponseFunction();
     private static final Logger LOG = LoggerFactory.getLogger(ToActionResponseFunction.class);
-    
+    private static final String COMMA = ",";
+    private static final String MESSAGE = "message";
+    private static final String FAILED_MSG = "Cannot '%s' while instance in in state of %s";
     
     @Override
     public ActionResponse apply(HttpResponse response) {
@@ -26,14 +28,14 @@ public class ToActionResponseFunction implements Function<HttpResponse, ActionRe
     public ActionResponse apply(HttpResponse response, String action) {
         if (response.getStatus() == 409)
         {
-            LOG.error(response.getStatus() + "," + response.getStatusMessage());
+            LOG.error(response.getStatus() + COMMA + response.getStatusMessage());
             if (action == null)
                 return ActionResponse.actionFailed("Instance currently is in build state");
-            return ActionResponse.actionFailed(String.format("Cannot '%s' while instance in in state of %s", action, action));
+            return ActionResponse.actionFailed(String.format(FAILED_MSG, action, action));
         }
         if (response.getStatus() >= 400 && response.getStatus() < 409) {
             String message = response.readEntity(String.class);
-            if (message != null && message.contains("message"))
+            if (message != null && message.contains(MESSAGE))
                 return ActionResponse.actionFailed(JsonToMessageFunction.INSTANCE.apply(message));
         }
         return ActionResponse.actionSuccess();
