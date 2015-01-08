1dbe4e45803ce639183c6b1cfb991e5c92ec1afa
magixyu <magix.yu@gmail.com>
2015-01-09 00:15:15 +0800

    Add the support for nested template

--- core/src/main/java/org/openstack4j/openstack/heat/utils/Template.java
+++ core/src/main/java/org/openstack4j/openstack/heat/utils/Template.java
@@ -15,7 +15,7 @@ import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 
 public class Template {
-
+    
     // template Resource is used for represent the template file or template URL
     private String tplContent;
     private Map<String, String> files = new HashMap<String, String>();
@@ -44,31 +44,78 @@ public class Template {
         @SuppressWarnings("unchecked")
         Map<String, Object> content = (Map<String, Object>) yaml.load(getTplContent());
         try {
-            findGetFile(content);
+            resolveTempalteGetFiles(content);
+            resolveTemplateType(content);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
     
-    private void findGetFile(Map<?,?> map) throws IOException {
+    private void resolveTemplateType(Map<?,?> map) throws MalformedURLException, IOException {
+        for(Object key : map.keySet()) {
+            // Ignore if the key is not string. Actually not happening
+            if(!(key instanceof String)) {
+                continue;
+            } 
+            
+            String skey = (String) key;
+            Object value = map.get(skey);
+
+            if(value instanceof String) {
+                String valueInString = (String) value;
+                //Processing the nested template
+                if(isTemplate(skey, valueInString)) {
+                    try {
+                        URL templateName =  TemplateUtils.normaliseFilePathToUrl(baseUrl + valueInString);
+
+                        if(! files.containsKey(templateName.toString())) {
+                            Template tpl = new Template(templateName);
+                            files.put(templateName.toString(),tpl.getTplContent());
+                            files.putAll(tpl.getFiles());
+                        }
+                    } catch (URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            
+            
+            if (value instanceof Map<?,?>){
+                resolveTemplateType((Map<?,?>)value);
+            } else if (value instanceof List<?>) {
+                for (Object item : (List<?>) value) {
+                    if (item instanceof Map<?, ?>) {
+                        resolveTemplateType((Map<?, ?>) item);
+                    }
+                }
+            }
+        }
+    }
+    
+    private void resolveTempalteGetFiles(Map<?,?> map) throws IOException  {
         for(Object key : map.keySet()){
+            // Ignore if the key is not string. Actually not happening
             if(!(key instanceof String)) {
                 continue;
             }
             
-            if(isGetFile((String)key)) {
-                String fileFullPath = baseUrl + (String)map.get(key);
+            String skey = (String) key;
+            Object value = map.get(skey);
+            
+            if(isGetFile(skey)) {
+                //if key="get_file", the value is the filename
+                String fileFullPath = baseUrl + (String)value;
                 addToFiles(fileFullPath);
                 continue;
             }
             
-            Object subMap = map.get(key);
+            Object subMap = map.get(skey);
             if (subMap instanceof Map<?,?>){
-                findGetFile((Map<?,?>)subMap);
+                resolveTempalteGetFiles((Map<?,?>)subMap);
             } else if (subMap instanceof List<?>) {
                 for (Object item : (List<?>) subMap) {
                     if (item instanceof Map<?, ?>) {
-                        findGetFile((Map<?, ?>) item);
+                        resolveTempalteGetFiles((Map<?, ?>) item);
                     }
                 }
             }
@@ -85,6 +132,16 @@ public class Template {
         return tag.equals(GET_FILE);
     }
     
+    private boolean isTemplate(String key, String value) {
+        if (! key.equals("type")) {
+            return false;
+        } 
+        if (value.endsWith(".yaml") || value.endsWith(".template")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     
     public String getTplContent() {
         return tplContent;
