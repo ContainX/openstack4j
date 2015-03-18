7b934edf6e14f6c84b5da4d5f27f9418551da898
Jeremy Unruh <jeremyunruh@sbcglobal.net>
2015-03-18 16:36:53 -0700

    Issue #290, re-enable and fix some LBaaS tests

--- core-test/src/main/java/org/openstack4j/api/network/LbPoolTests.java
+++ core-test/src/main/java/org/openstack4j/api/network/LbPoolTests.java
@@ -2,8 +2,10 @@ package org.openstack4j.api.network;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,38 +19,42 @@ import org.openstack4j.model.network.ext.LbPool;
 import org.openstack4j.model.network.ext.LbPoolUpdate;
 import org.openstack4j.openstack.networking.domain.ext.LbMethod;
 import org.openstack4j.openstack.networking.domain.ext.Protocol;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-/**
- * 
- * @author liujunpeng
- *
- */
-@Test(suiteName="Network/lbpool", enabled=false)
-public class LbPoolTests extends AbstractTest{
-	public void testListPool() {
+@Test(suiteName="Network/lbpool", enabled=true)
+public class LbPoolTests extends AbstractTest {
+
+    private static final String LBPOOLS_JSON = "/network/lbpools.json";
+    private static final String LBPOOL_JSON = "/network/lbpool.json";
+    private static final String LBPOOL_UPDATE_JSON = "/network/lbpool_update.json";
+    
+	public void testListPool() throws IOException {
+	    respondWith(LBPOOLS_JSON);
 		List<? extends LbPool> list = os().networking().loadbalancers().lbPool().list();
-		System.out.println("test lb pool List" + list);
 		assertEquals(1, list.size());
+		assertEquals(list.get(0).getSubnetId(), "8032909d-47a1-4715-90af-5153ffe39861");
 	}
 
-	public void testListPoolFilter() {
+	public void testListPoolFilter() throws IOException {
+	    respondWith(LBPOOLS_JSON);
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("name", "pool");
 		List<? extends LbPool> list = os().networking().loadbalancers().lbPool().list(map);
-		System.out.println("test lb pool List filter" + list);
 		assertEquals(1, list.size());
 	}
 
-	public void testGetPool() {
-		String id = "f6f1a5bb-2c5d-4c41-941c-024eb39414a6";
+	public void testGetPool() throws IOException {
+	    respondWith(LBPOOL_JSON);
+		String id = "4c0a0a5f-cf8f-44b7-b912-957daa8ce5e5";
 		LbPool pool = os().networking().loadbalancers().lbPool().get(id);
-		System.out.println("test get a pool" + pool);
+		assertNotNull(pool);
 		assertEquals(id, pool.getId());
 	}
 
-	public void testCreatePool() {
-		String name = "create";
+	public void testCreatePool() throws IOException {
+	    respondWith(LBPOOL_JSON);
+		String name = "pool1";
 		String protocol = Protocol.HTTP.toString();
 		LbPool create = Builders.lbPool().adminStateUp(true)
 				.description("pool").lbMethod(LbMethod.ROUND_ROBIN.toString())
@@ -58,11 +64,11 @@ public class LbPoolTests extends AbstractTest{
 				.protocol(protocol).build();
 		LbPool result = os().networking().loadbalancers().lbPool().create(create);
 		assertEquals(name, result.getName());
-		assertEquals(LbMethod.ROUND_ROBIN.toString(), result.getLbMethod());
+		assertEquals(result.getLbMethod(), LbMethod.ROUND_ROBIN.toString());
 		assertEquals(protocol, result.getProtocol());
 	}
-
-	public void testUpdatePool() {
+	public void testUpdatePool() throws IOException {
+	    respondWith(LBPOOL_UPDATE_JSON);
 		String poolId = "db083bf7-c455-4758-b1ad-203cf441a73a";
 		String name = "update";
 		LbPoolUpdate update = Builders.lbPoolUpdate().adminStateUp(false)
@@ -76,6 +82,7 @@ public class LbPoolTests extends AbstractTest{
 		assertFalse(result.isAdminStateUp());
 	}
 
+	@Test(enabled=false)
 	public void testAssociateHealthMonitor() {
 		String poolId = "db083bf7-c455-4758-b1ad-203cf441a73a";
 		String healthId = "8767284a-b542-4db6-8393-0a404a959c1d";
@@ -83,7 +90,8 @@ public class LbPoolTests extends AbstractTest{
 		HealthMonitor result = os().networking().loadbalancers().lbPool().associateHealthMonitor(poolId, associate);
 		assertTrue(result != null);
 	}
-
+	
+    @Test(enabled=false)
 	public void testDisAssociateHealthMonitor() {
 		String poolId = "db083bf7-c455-4758-b1ad-203cf441a73a";
 		String healthId = "8767284a-b542-4db6-8393-0a404a959c1d";
@@ -91,13 +99,13 @@ public class LbPoolTests extends AbstractTest{
 				.disAssociateHealthMonitor(poolId, healthId);
 		assertTrue(result.isSuccess());
 	}
-
+	
 	public void testDeletePool() {
-		String id = "02ae87ec-5502-469e-8c41-f2c57d185054";
-		ActionResponse result = os().networking().loadbalancers().lbPool().delete(id);
-		System.out.println("delete a pool" + result);
+	    respondWith(200);
+		ActionResponse result = os().networking().loadbalancers().lbPool().delete("02ae87ec-5502-469e-8c41-f2c57d185054");
 		assertTrue(result.isSuccess());
 	}
+	
 	@Override
 	protected Service service() {
 		return Service.NETWORK;
