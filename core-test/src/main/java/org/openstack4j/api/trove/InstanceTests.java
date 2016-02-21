e5401651f2062c334a1e4f4404d0eff8504c3d19
Craig Vyvial <cp16net@gmail.com>
2016-02-21 00:13:49 -0600

    adding trove support
    * add trove flavor to api
    * added tests for flavor
    
    WIP
    * adding create instance

--- core-test/src/main/java/org/openstack4j/api/trove/InstanceTests.java
+++ core-test/src/main/java/org/openstack4j/api/trove/InstanceTests.java
@@ -1,7 +1,10 @@
 package org.openstack4j.api.trove;
 
 import org.openstack4j.api.AbstractTest;
+import org.openstack4j.api.Builders;
+import org.openstack4j.api.exceptions.ServerResponseException;
 import org.openstack4j.model.trove.Instance;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -24,10 +27,27 @@ public class InstanceTests extends AbstractTest {
         assertEquals(1, instances.size());
 
         Instance i = instances.get(0);
-//        assertEquals(1, i.getAddresses().getAddresses("private").size());
-//        assertEquals("192.168.0.3", i.getAddresses().getAddresses("private").get(0).getAddr());
         assertEquals(Instance.Status.ACTIVE, i.getStatus());
         assertEquals("instance-1", i.getName());
+    }
+
+    @Test(expectedExceptions = ServerResponseException.class, invocationCount = 10)
+    public void instanceError() {
+        String jsonResponse = "{\"itemNotFound\": {"
+                + "\"message\": \"Resource test cannot be found.\", "
+                + "\"code\": 404}}";
+
+        respondWith(404, jsonResponse);
+        os().trove().instances().get("05184ba3-00ba-4fbc-b7a2-03b62b884931");
+        Assert.fail("Exception should have been thrown.");
+    }
+
+    @Test
+    public void createInstance() throws Exception {
+        respondWith(JSON_INSTANCE_CREATE);
+
+        Server server = os().trove().instances().create(Builders.server().name("server-test-1").build());
+        assertEquals("server-test-1", server.getName());
     }
 
     @Override
--- core/src/main/java/org/openstack4j/api/trove/FlavorService.java
+++ core/src/main/java/org/openstack4j/api/trove/FlavorService.java
@@ -10,4 +10,5 @@ import java.util.List;
  */
 public interface FlavorService {
     List<? extends Flavor> list();
+    Flavor get(String id);
 }
