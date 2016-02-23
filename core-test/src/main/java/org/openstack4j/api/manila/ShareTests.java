4967b4e436bc9bd4e47a5531417ffb465fba1bfa
Daniel Gonzalez <daniel@gonzalez-nothnagel.de>
2016-02-23 18:42:43 +0100

    Implement manila services

--- core-test/src/main/java/org/openstack4j/api/manila/ShareTests.java
+++ core-test/src/main/java/org/openstack4j/api/manila/ShareTests.java
@@ -6,14 +6,14 @@ import org.openstack4j.model.common.Extension;
 import org.openstack4j.model.manila.AbsoluteLimit;
 import org.openstack4j.model.manila.Limits;
 import org.openstack4j.model.manila.RateLimit;
+import org.openstack4j.openstack.manila.domain.ManilaService;
 import org.testng.annotations.Test;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 /**
  * Test cases for share services
@@ -24,12 +24,15 @@ import static org.testng.Assert.assertTrue;
 public class ShareTests extends AbstractTest {
     private static final String JSON_EXTENSIONS = "/manila/extensions.json";
     private static final String JSON_LIMITS = "/manila/limits.json";
+    private static final String JSON_SERVICES = "/manila/os-services.json";
+    private static final String JSON_SERVICE_ENABLE = "/manila/os-services_enable.json";
+    private static final String JSON_SERVICE_DISABLE = "/manila/os-services_disable.json";
 
     private static final DateFormat ISO8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
 
     @Override
     protected Service service() {
-        return Service.SHARE;
+        return AbstractTest.Service.SHARE;
     }
 
     @Test
@@ -69,5 +72,54 @@ public class ShareTests extends AbstractTest {
         assertEquals(absoluteLimit.getTotalSnapshotGigabytesUsed(), 0);
         assertEquals(absoluteLimit.getMaxTotalSnapshotGigabytes(), 1000);
         assertEquals(absoluteLimit.getMaxTotalShareSnapshots(), 50);
+    }
+
+    @Test
+    public void services() throws Exception {
+        respondWith(JSON_SERVICES);
+
+        List<? extends org.openstack4j.model.manila.Service> services = os().share().services();
+        assertEquals(services.size(), 2);
+
+        org.openstack4j.model.manila.Service service1 = services.get(0);
+        org.openstack4j.model.manila.Service service2 = services.get(1);
+
+        assertEquals(service1.getStatus(), org.openstack4j.model.manila.Service.Status.ENABLED);
+        assertEquals(service1.getBinary(), "manila-share");
+        assertEquals(service1.getZone(), "nova");
+        assertEquals(service1.getHost(), "manila2@generic1");
+        assertEquals(service1.getUpdatedAt(), "2015-09-07T13:03:57.000000");
+        assertEquals(service1.getState(), org.openstack4j.model.manila.Service.State.UP);
+        assertEquals((int) service1.getId(), 1);
+
+        assertEquals(service2.getStatus(), org.openstack4j.model.manila.Service.Status.ENABLED);
+        assertEquals(service2.getBinary(), "manila-scheduler");
+        assertEquals(service2.getZone(), "nova");
+        assertEquals(service2.getHost(), "manila2");
+        assertEquals(service2.getUpdatedAt(), "2015-09-07T13:03:57.000000");
+        assertEquals(service2.getState(), org.openstack4j.model.manila.Service.State.UP);
+        assertEquals((int) service2.getId(), 2);
+    }
+
+    @Test
+    public void enableService() throws Exception {
+        respondWith(JSON_SERVICE_ENABLE);
+
+        ManilaService.ServiceStatus status = os().share().enableService("manila-share", "openstack@cmode");
+
+        assertFalse(status.getDisabled());
+        assertEquals(status.getBinary(), "manila-share");
+        assertEquals(status.getHost(), "openstack@cmode");
+    }
+
+    @Test
+    public void disableService() throws Exception {
+        respondWith(JSON_SERVICE_DISABLE);
+
+        ManilaService.ServiceStatus status = os().share().disableService("manila-share", "openstack@cmode");
+
+        assertTrue(status.getDisabled());
+        assertEquals(status.getBinary(), "manila-share");
+        assertEquals(status.getHost(), "openstack@cmode");
     }
 }
--- core/src/main/java/org/openstack4j/openstack/manila/internal/ShareServiceImpl.java
+++ core/src/main/java/org/openstack4j/openstack/manila/internal/ShareServiceImpl.java
@@ -4,10 +4,15 @@ import org.openstack4j.api.Apis;
 import org.openstack4j.api.manila.*;
 import org.openstack4j.model.common.Extension;
 import org.openstack4j.model.manila.Limits;
+import org.openstack4j.model.manila.Service;
 import org.openstack4j.openstack.common.ExtensionValue.ManilaExtensions;
 import org.openstack4j.openstack.manila.domain.ManilaLimits;
+import org.openstack4j.openstack.manila.domain.ManilaService;
+import org.openstack4j.openstack.manila.domain.actions.ServiceAction;
 
 import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Shared File Systems API (Manila)
@@ -76,5 +81,39 @@ public class ShareServiceImpl extends BaseShareServices implements ShareService 
     @Override
     public SchedulerStatsService schedulerStats() {
         return Apis.get(SchedulerStatsService.class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<? extends Service> services() {
+        return get(ManilaService.Services.class, uri("/os-services")).execute().getList();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ManilaService.ServiceStatus enableService(String binary, String host) {
+        checkNotNull(binary);
+        checkNotNull(host);
+
+        return put(ManilaService.ServiceStatus.class, uri("/os-services/enable"))
+                .entity(ServiceAction.enable(binary, host))
+                .execute();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ManilaService.ServiceStatus disableService(String binary, String host) {
+        checkNotNull(binary);
+        checkNotNull(host);
+
+        return put(ManilaService.ServiceStatus.class, uri("/os-services/disable"))
+                .entity(ServiceAction.disable(binary, host))
+                .execute();
     }
 }
