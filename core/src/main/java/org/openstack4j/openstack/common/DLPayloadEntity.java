f6df4ecc9c2b42614bfc4f9423ee479062d6f72e
vinod <viborole@cisco.com>
2016-05-23 20:11:10 +0530

    Closing input stream, ref#622

--- core/src/main/java/org/openstack4j/openstack/common/DLPayloadEntity.java
+++ core/src/main/java/org/openstack4j/openstack/common/DLPayloadEntity.java
@@ -42,8 +42,11 @@ public class DLPayloadEntity implements DLPayload {
     @Override
     public void writeToFile(File file) throws IOException {
         checkNotNull(file);
-
-        ByteStreams.copy(response.getInputStream(), new FileOutputStream(file));
+        try{
+            ByteStreams.copy(response.getInputStream(), new FileOutputStream(file));
+        }finally{
+            response.getInputStream().close();
+        }
     }
 
 }
