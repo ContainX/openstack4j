80242c356590be3f0cfde0a27690beacb025cd5e
Emily Burns <emjeburns@gmail.com>
2016-07-12 15:11:36 -0700

    proposed apis for lbaas v2 support

---
+++ core/src/main/java/org/openstack4j/api/networking/ext/LbPoolV2Service.java
@@ -0,0 +1,11 @@
+package org.openstack4j.api.networking.ext;
+
+import org.openstack4j.common.RestService;
+
+/**
+ * Networking (Neutron) Lbaas V2 pool Extension API
+ * @author emjburns
+ */
+public interface LbPoolV2Service extends RestService {
+    // TODO: add support for lbaas v2 pools
+}
---
+++ core/src/main/java/org/openstack4j/api/networking/ext/ListenerService.java
@@ -0,0 +1,11 @@
+package org.openstack4j.api.networking.ext;
+
+import org.openstack4j.common.RestService;
+
+/**
+ * Networking (Neutron) Lbaas V2 listener Extension API
+ * @author emjburns
+ */
+public interface ListenerService extends RestService {
+    // TODO: add support for lbaas v2 listeners
+}
