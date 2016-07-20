0596307aa31e9d7b20b6afe01ee3bafd92896301
emjburns <emjeburns@gmail.com>
2016-07-20 14:21:32 -0700

    linking service apis to impls

--- core/src/main/java/org/openstack4j/openstack/networking/internal/ext/ListenerServiceImpl.java
+++ core/src/main/java/org/openstack4j/openstack/networking/internal/ext/ListenerServiceImpl.java
@@ -18,6 +18,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * @author emjburns
  */
 public class ListenerServiceImpl extends BaseNetworkingServices implements ListenerService{
+
     /**
      * {@inheritDoc}
      */
