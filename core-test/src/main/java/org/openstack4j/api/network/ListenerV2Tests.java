51e501ac1caf49d14c69a0cda9d2fc390293d374
ashleykasim <ashleykasim1@gmail.com>
2016-07-27 17:19:04 -0400

    additional loadbalancer tests, listener -> listener v2

--- core-test/src/main/java/org/openstack4j/api/network/ListenerV2Tests.java
+++ core-test/src/main/java/org/openstack4j/api/network/ListenerV2Tests.java
@@ -24,37 +24,37 @@ import static org.testng.Assert.assertFalse;
  * @author ashleykasim
  *
  */
-@Test(suiteName="Network/listener", enabled = true)
-public class ListenerTests extends AbstractTest {
-    private static final String LISTENERS_JSON = "/network/listeners.json";
-    private static final String LISTENER_JSON = "/network/listener.json";
-    private static final String LISTENER_UPDATE_JSON = "/network/listener_update.json";
+@Test(suiteName="Network/listenerV2", enabled = true)
+public class ListenerV2Tests extends AbstractTest {
+    private static final String LISTENERSV2_JSON = "/network/listenersv2.json";
+    private static final String LISTENERV2_JSON = "/network/listenerv2.json";
+    private static final String LISTENERV2_UPDATE_JSON = "/network/listenerv2_update.json";
 
-    public void testListListeners() throws IOException {
-        respondWith(LISTENERS_JSON);
+    public void testListListenersV2() throws IOException {
+        respondWith(LISTENERSV2_JSON);
         List<? extends ListenerV2> list = osv3().networking().lbaasV2().listenerV2().list();
         assertEquals(list.size(), 2);
         assertEquals(list.get(0).getName(), "listener1");
     }
 
-    public void testListListenersFilter() throws IOException {
-        respondWith(LISTENERS_JSON);
+    public void testListListenersV2Filter() throws IOException {
+        respondWith(LISTENERSV2_JSON);
         Map<String, String> map = new HashMap<>();
         map.put("tenantId", "6f759d84e3ca496ab77f8c0ffaa0311e");
         List<? extends ListenerV2> list = osv3().networking().lbaasV2().listenerV2().list(map);
         assertEquals(list.size(), 2);
     }
 
-    public void testGetlistener() throws IOException {
-        respondWith(LISTENER_JSON);
+    public void testGetListenerV2() throws IOException {
+        respondWith(LISTENERV2_JSON);
         String id = "c07058a9-8d84-4443-b8f5-508d0facfe10";
         ListenerV2 listener = osv3().networking().lbaasV2().listenerV2().get(id);
         assertNotNull(listener);
         assertEquals(listener.getId(), id);
     }
 
-    public void testCreatelistener() throws IOException {
-        respondWith(LISTENER_JSON);
+    public void testCreateListenerV2() throws IOException {
+        respondWith(LISTENERV2_JSON);
         String name = "listener1";
         String description = "";
         Protocol protocol = Protocol.HTTP;
@@ -71,8 +71,8 @@ public class ListenerTests extends AbstractTest {
         assertTrue(result.isAdminStateUp());
     }
 
-    public void testUpdatelistener() throws IOException {
-        respondWith(LISTENER_UPDATE_JSON);
+    public void testUpdateListenerV2() throws IOException {
+        respondWith(LISTENERV2_UPDATE_JSON);
         String name = "listener_updated";
         String description = "im a good listener";
         Integer connectionLimit = 20;
@@ -89,7 +89,7 @@ public class ListenerTests extends AbstractTest {
         assertEquals(result.getConnectionLimit(), connectionLimit);
     }
 
-    public void testDeletelistener() {
+    public void testDeleteListenerV2() {
         respondWith(204);
         ActionResponse result = osv3().networking().lbaasV2().listenerV2().delete("c07058a9-8d84-4443-b8f5-508d0facfe10");
         assertTrue(result.isSuccess());
