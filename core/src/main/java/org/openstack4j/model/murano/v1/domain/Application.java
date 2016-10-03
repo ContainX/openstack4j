4d0425efcf463ddd3b04c7fd0f8026db0358c0a5
Nikolay Mahotkin <nmakhotkin@mirantis.com>
2016-10-03 12:17:42 +0300

    [Murano] Removing unused Builder classes

--- core/src/main/java/org/openstack4j/model/murano/v1/domain/Application.java
+++ core/src/main/java/org/openstack4j/model/murano/v1/domain/Application.java
@@ -1,24 +1,14 @@
 package org.openstack4j.model.murano.v1.domain;
 
-import org.openstack4j.common.Buildable;
 import org.openstack4j.model.ModelEntity;
-import org.openstack4j.model.murano.v1.builder.ApplicationBuilder;
-
-import java.util.Map;
 
 /**
  * @author Nikolay Mahotkin.
  */
-public interface Application extends ModelEntity/*, Buildable<ApplicationBuilder>*/ {
+public interface Application extends ModelEntity {
     /**
      *
      * @return data in raw format (json-string).
      */
     Object getData();
-
-    /**
-     *
-     * @return data in map model.
-     */
-    //Map<String, Object> getDataMap();
 }
