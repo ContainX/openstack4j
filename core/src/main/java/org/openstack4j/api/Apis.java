479afa798f1f25af569669c59fb92865d451c5cb
Bart Cornelis <bart.cornelis@gmail.com>
2016-11-02 14:23:20 +0100

    revert privious implementation using the API endpoint method

--- core/src/main/java/org/openstack4j/api/Apis.java
+++ core/src/main/java/org/openstack4j/api/Apis.java
@@ -4,7 +4,6 @@ import org.openstack4j.api.barbican.BarbicanService;
 import org.openstack4j.api.compute.ComputeService;
 import org.openstack4j.api.gbp.GbpService;
 import org.openstack4j.api.heat.HeatService;
-import org.openstack4j.api.identity.EndpointURLResolver;
 import org.openstack4j.api.image.ImageService;
 import org.openstack4j.api.manila.ShareService;
 import org.openstack4j.api.murano.v1.AppCatalogService;
@@ -167,14 +166,6 @@ public class Apis {
      */
     public static BarbicanService getBarbicanServices() {
         return get(BarbicanService.class);
-    }
-    
-    /**
-     * Gets the endpoint URL resolver services API
-     * @return the EndpointURLResolver services
-     */
-    public static EndpointURLResolver getEndpointURLResolver() {
-    	return get(EndpointURLResolver.class);
     }
 
     private static APIProvider initializeProvider() {
