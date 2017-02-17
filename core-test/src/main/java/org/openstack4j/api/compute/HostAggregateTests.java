d10905133f2d6dbcce1674f987e8be49e2cbaea4
Cherry522 <522581280@qq.com>
2017-02-17 15:35:16 +0800

    add hostAggregates list test

---
+++ core-test/src/main/java/org/openstack4j/api/compute/HostAggregateTests.java
@@ -0,0 +1,39 @@
+package org.openstack4j.api.compute;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.List;
+
+import org.openstack4j.api.AbstractTest;
+import org.openstack4j.model.compute.HostAggregate;
+import org.testng.annotations.Test;
+
+/**
+ * Host Aggregate Tests
+ * @author chenyan
+ *
+ */
+@Test(suiteName = "HostAggregate")
+public class HostAggregateTests extends AbstractTest {
+	
+	private static final String JSON_HOST_AGGREGATES = "/compute/aggregates.json";
+	
+	@Test
+	public void serviceListingTest() throws Exception {
+        respondWith(JSON_HOST_AGGREGATES);
+
+        List<? extends HostAggregate> hostAggregateList = osv3().compute().hostAggregates().list();
+        assertEquals(hostAggregateList.size(),2);
+
+        HostAggregate aggregate = hostAggregateList.get(0);
+        assertEquals(aggregate.getAvailabilityZone(),"uec_zone_1");
+        assertEquals(aggregate.getId(),"8");
+        assertEquals(aggregate.getName(),"aggregate_zl_test");
+    }
+
+	@Override
+	protected Service service() {
+		return Service.COMPUTE;
+	}
+
+}
