huaweicloudsdk/sdk-java

Mistral: Implement workbook definitions endpoint

core-test/src/main/java/org/openstack4j/api/workflow/WorkbookDefinitionTest.java
@@ -1,0 +1,117 @@
+package org.openstack4j.api.workflow;
+
+import org.openstack4j.core.transport.HttpResponse;
+import org.openstack4j.model.workflow.Scope;
+import org.openstack4j.model.workflow.WorkbookDefinition;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+/**
+ * Test cases for {@link WorkbookDefinitionService}.
+ * 
+ * @author Renat Akhmerov
+ */
+@Test(suiteName="WorkbookDefinitions")
+public class WorkbookDefinitionTest extends WorkflowBaseTest {
+
+    private static final String JSON_WB_DEF = "/workflow/wb_def.json";
+    private static final String JSON_WB_DEFS = "/workflow/wb_defs.json";
+    private static final String NEW_WB = "/workflow/new_wb.yaml";
+    private static final String JSON_WB_DEF_CREATE = "/workflow/wb_def_create.json";
+
+    private WorkbookDefinitionService service;
+
+    @BeforeTest
+    public void setUp() {
+        this.service = osv3().workflow().workbookDefinitions();
+    }
+
+    @Test
+    public void listWorkbookDefinitions() throws Exception {
+        respondWith(JSON_WB_DEFS);
+        
+        List<? extends WorkbookDefinition> wbDefs = service.list();
+
+        assertEquals(wbDefs.size(), 2);
+
+        // Check first workbook definition.
+        WorkbookDefinition wbDef = wbDefs.get(0);
+
+        assertNotNull(wbDef);
+        assertIsUUID(wbDef.getId());
+        assertNotEmptyString(wbDef.getProjectId());
+        assertEquals(wbDef.getName(), "my_wb0");
+        assertNotNull(wbDef.getTags());
+        assertEquals(wbDef.getTags().get(0),"test");
+        assertEquals(wbDef.getTags().get(1), "private");
+        assertNotNull(wbDef.getCreatedAt());
+        assertNull(wbDef.getUpdatedAt());
+        assertNotEmptyString(wbDef.getDefinition());
+        assertTrue(wbDef.getDefinition().contains("join: all"));
+
+        // Check second workbook definition.
+        wbDef = wbDefs.get(1);
+
+        assertNotNull(wbDef);
+        assertIsUUID(wbDef.getId());
+        assertNotEmptyString(wbDef.getProjectId());
+        assertEquals(wbDef.getName(), "my_wb1");
+        assertNotNull(wbDef.getTags());
+        assertEquals(wbDef.getTags().size(), 0);
+        assertNotNull(wbDef.getCreatedAt());
+        assertNull(wbDef.getUpdatedAt());
+        assertNotEmptyString(wbDef.getDefinition());
+        assertTrue(wbDef.getDefinition().contains("with-items: i in <% range(0, 40) %>"));
+    }
+
+    @Test
+    public void getWorkbookDefinition() throws Exception {
+        respondWith(JSON_WB_DEF);
+
+        WorkbookDefinition wbDef = service.get("eecf6cad-65af-4a11-9e6f-692b23ffac08");
+
+        assertNotNull(wbDef);
+        assertIsUUID(wbDef.getId());
+        assertNotEmptyString(wbDef.getProjectId());
+        assertEquals(wbDef.getName(), "my_wb");
+        assertNotNull(wbDef.getTags());
+        assertEquals(wbDef.getTags().get(0), "test");
+        assertEquals(wbDef.getTags().get(1), "private");
+        assertNotNull(wbDef.getCreatedAt());
+        assertNull(wbDef.getUpdatedAt());
+        assertNotEmptyString(wbDef.getDefinition());
+        assertTrue(wbDef.getDefinition().contains("with-items: i in <% range(0, 40) %>"));
+    }
+
+    @Test
+    public void createWorkbookDefinition() throws Exception {
+        respondWith(JSON_WB_DEF_CREATE);
+
+        WorkbookDefinition wbDef = service.create(getClass().getResourceAsStream(NEW_WB), Scope.PRIVATE);
+
+        assertNotNull(wbDef);
+        assertIsUUID(wbDef.getId());
+        assertNotEmptyString(wbDef.getProjectId());
+        assertEquals(wbDef.getName(), "my_wb");
+        assertNotNull(wbDef.getTags());
+        assertEquals(wbDef.getTags().get(0), "test");
+        assertEquals(wbDef.getTags().get(1), "private");
+        assertNotNull(wbDef.getCreatedAt());
+        assertNull(wbDef.getUpdatedAt());
+        assertNotEmptyString(wbDef.getDefinition());
+        assertTrue(wbDef.getDefinition().contains("with-items: i in <% range(0, 40) %>"));
+    }
+
+    @Test
+    public void deleteWorkbookDefinition() throws Exception {
+        respondWith(204); // No content.
+
+        HttpResponse resp = service.delete("my_wb");
+
+        assertEquals(resp.getStatus(), 204);
+    }
+}

core/src/main/java/org/openstack4j/openstack/workflow/internal/WorkbookDefinitionServiceImpl.java
@@ -1,0 +1,51 @@
+package org.openstack4j.openstack.workflow.internal;
+
+import org.openstack4j.api.workflow.WorkbookDefinitionService;
+import org.openstack4j.core.transport.HttpResponse;
+import org.openstack4j.model.common.payloads.InputStreamPayload;
+import org.openstack4j.model.workflow.Scope;
+import org.openstack4j.model.workflow.WorkbookDefinition;
+import org.openstack4j.openstack.workflow.domain.MistralWorkbookDefinition;
+import org.openstack4j.openstack.workflow.domain.MistralWorkbookDefinition.MistralWorkbookDefinitions;
+
+import java.io.InputStream;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Workbook definition service implementation.
+ *
+ * @author Renat Akhmerov
+ */
+public class WorkbookDefinitionServiceImpl extends BaseMistralService implements WorkbookDefinitionService {
+
+    @Override
+    public List<? extends WorkbookDefinition> list() {
+        return get(MistralWorkbookDefinitions.class, uri("/workbooks")).execute().getList();
+    }
+
+    @Override
+    public WorkbookDefinition create(InputStream wfText, Scope scope) {
+        checkNotNull(wfText);
+        checkNotNull(scope);
+
+        Invocation<MistralWorkbookDefinition> invocation = post(
+                MistralWorkbookDefinition.class,
+                uri("/workbooks?scope=%s", scope.toString().toLowerCase())
+        );
+
+        return invocation.entity(new InputStreamPayload(wfText)).execute();
+    }
+
+    @Override
+    public WorkbookDefinition get(String identifier) {
+        return get(MistralWorkbookDefinition.class, uri("/workbooks/%s", identifier)).execute();
+    }
+
+    @Override
+    public HttpResponse delete(String identifier) {
+        return deleteWithResponse(uri("/workbooks/%s", identifier)).executeWithResponse();
+    }
+
+}

ContainX/openstack4j

core-test/src/main/java/org/openstack4j/api/workflow/WorkbookDefinitionTest.java
@@ -1,0 +1,118 @@
+package org.openstack4j.api.workflow;
+
+import org.openstack4j.model.common.ActionResponse;
+import org.openstack4j.model.workflow.Scope;
+import org.openstack4j.model.workflow.WorkbookDefinition;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+/**
+ * Test cases for {@link WorkbookDefinitionService}.
+ * 
+ * @author Renat Akhmerov
+ */
+@Test(suiteName="WorkbookDefinitions")
+public class WorkbookDefinitionTest extends WorkflowBaseTest {
+
+    private static final String JSON_WB_DEF = "/workflow/wb_def.json";
+    private static final String JSON_WB_DEFS = "/workflow/wb_defs.json";
+    private static final String NEW_WB = "/workflow/new_wb.yaml";
+    private static final String JSON_WB_DEF_CREATE = "/workflow/wb_def_create.json";
+
+    private WorkbookDefinitionService service;
+
+    @BeforeTest
+    public void setUp() {
+        this.service = osv3().workflow().workbookDefinitions();
+    }
+
+    @Test
+    public void listWorkbookDefinitions() throws Exception {
+        respondWith(JSON_WB_DEFS);
+        
+        List<? extends WorkbookDefinition> wbDefs = service.list();
+
+        assertEquals(wbDefs.size(), 2);
+
+        // Check first workbook definition.
+        WorkbookDefinition wbDef = wbDefs.get(0);
+
+        assertNotNull(wbDef);
+        assertIsUUID(wbDef.getId());
+        assertNotEmptyString(wbDef.getProjectId());
+        assertEquals(wbDef.getName(), "my_wb0");
+        assertNotNull(wbDef.getTags());
+        assertEquals(wbDef.getTags().get(0),"test");
+        assertEquals(wbDef.getTags().get(1), "private");
+        assertNotNull(wbDef.getCreatedAt());
+        assertNull(wbDef.getUpdatedAt());
+        assertNotEmptyString(wbDef.getDefinition());
+        assertTrue(wbDef.getDefinition().contains("join: all"));
+
+        // Check second workbook definition.
+        wbDef = wbDefs.get(1);
+
+        assertNotNull(wbDef);
+        assertIsUUID(wbDef.getId());
+        assertNotEmptyString(wbDef.getProjectId());
+        assertEquals(wbDef.getName(), "my_wb1");
+        assertNotNull(wbDef.getTags());
+        assertEquals(wbDef.getTags().size(), 0);
+        assertNotNull(wbDef.getCreatedAt());
+        assertNull(wbDef.getUpdatedAt());
+        assertNotEmptyString(wbDef.getDefinition());
+        assertTrue(wbDef.getDefinition().contains("with-items: i in <% range(0, 40) %>"));
+    }
+
+    @Test
+    public void getWorkbookDefinition() throws Exception {
+        respondWith(JSON_WB_DEF);
+
+        WorkbookDefinition wbDef = service.get("eecf6cad-65af-4a11-9e6f-692b23ffac08");
+
+        assertNotNull(wbDef);
+        assertIsUUID(wbDef.getId());
+        assertNotEmptyString(wbDef.getProjectId());
+        assertEquals(wbDef.getName(), "my_wb");
+        assertNotNull(wbDef.getTags());
+        assertEquals(wbDef.getTags().get(0), "test");
+        assertEquals(wbDef.getTags().get(1), "private");
+        assertNotNull(wbDef.getCreatedAt());
+        assertNull(wbDef.getUpdatedAt());
+        assertNotEmptyString(wbDef.getDefinition());
+        assertTrue(wbDef.getDefinition().contains("with-items: i in <% range(0, 40) %>"));
+    }
+
+    @Test
+    public void createWorkbookDefinition() throws Exception {
+        respondWith(JSON_WB_DEF_CREATE);
+
+        WorkbookDefinition wbDef = service.create(getClass().getResourceAsStream(NEW_WB), Scope.PRIVATE);
+
+        assertNotNull(wbDef);
+        assertIsUUID(wbDef.getId());
+        assertNotEmptyString(wbDef.getProjectId());
+        assertEquals(wbDef.getName(), "my_wb");
+        assertNotNull(wbDef.getTags());
+        assertEquals(wbDef.getTags().get(0), "test");
+        assertEquals(wbDef.getTags().get(1), "private");
+        assertNotNull(wbDef.getCreatedAt());
+        assertNull(wbDef.getUpdatedAt());
+        assertNotEmptyString(wbDef.getDefinition());
+        assertTrue(wbDef.getDefinition().contains("with-items: i in <% range(0, 40) %>"));
+    }
+
+    @Test
+    public void deleteWorkbookDefinition() throws Exception {
+        respondWith(204); // No content.
+
+        ActionResponse resp = service.delete("my_wb");
+
+        // TODO(rakhmerov): Change to 204 once ActionResponse can return other 2xx codes.
+        assertEquals(resp.getCode(), 200);
+    }
+}

core/src/main/java/org/openstack4j/openstack/workflow/internal/WorkbookDefinitionServiceImpl.java
@@ -1,0 +1,50 @@
+package org.openstack4j.openstack.workflow.internal;
+
+import org.openstack4j.api.workflow.WorkbookDefinitionService;
+import org.openstack4j.model.common.ActionResponse;
+import org.openstack4j.model.common.payloads.InputStreamPayload;
+import org.openstack4j.model.workflow.Scope;
+import org.openstack4j.model.workflow.WorkbookDefinition;
+import org.openstack4j.openstack.workflow.domain.MistralWorkbookDefinition;
+import org.openstack4j.openstack.workflow.domain.MistralWorkbookDefinition.MistralWorkbookDefinitions;
+
+import java.io.InputStream;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Workbook definition service implementation.
+ *
+ * @author Renat Akhmerov
+ */
+public class WorkbookDefinitionServiceImpl extends BaseMistralService implements WorkbookDefinitionService {
+
+    @Override
+    public List<? extends WorkbookDefinition> list() {
+        return get(MistralWorkbookDefinitions.class, uri("/workbooks")).execute().getList();
+    }
+
+    @Override
+    public WorkbookDefinition create(InputStream wfText, Scope scope) {
+        checkNotNull(wfText);
+        checkNotNull(scope);
+
+        Invocation<MistralWorkbookDefinition> invocation = post(
+                MistralWorkbookDefinition.class,
+                uri("/workbooks?scope=%s", scope.toString().toLowerCase())
+        );
+
+        return invocation.entity(new InputStreamPayload(wfText)).execute();
+    }
+
+    @Override
+    public WorkbookDefinition get(String identifier) {
+        return get(MistralWorkbookDefinition.class, uri("/workbooks/%s", identifier)).execute();
+    }
+
+    @Override
+    public ActionResponse delete(String identifier) {
+        return deleteWithResponse(uri("/workbooks/%s", identifier)).execute();
+    }
+}