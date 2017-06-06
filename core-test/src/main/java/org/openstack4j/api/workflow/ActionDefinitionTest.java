huaweicloudsdk/sdk-java

Mistral: Implement actions definitions endpoint

core-test/src/main/java/org/openstack4j/api/workflow/ActionDefinitionTest.java
@@ -1,0 +1,128 @@
+package org.openstack4j.api.workflow;
+
+import org.openstack4j.core.transport.HttpResponse;
+import org.openstack4j.model.workflow.ActionDefinition;
+import org.openstack4j.model.workflow.Scope;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+/**
+ * Test cases for {@link ActionDefinitionService}.
+ * 
+ * @author Renat Akhmerov
+ */
+@Test(suiteName="ActionDefinitions")
+public class ActionDefinitionTest extends WorkflowBaseTest {
+
+    private static final String JSON_ACTION_DEF = "/workflow/action_def.json";
+    private static final String JSON_ACTION_DEFS = "/workflow/action_defs.json";
+    private static final String NEW_ACTION = "/workflow/new_action.yaml";
+    private static final String JSON_ACTION_DEF_CREATE = "/workflow/action_def_create.json";
+
+    private ActionDefinitionService service;
+
+    @BeforeTest
+    public void setUp() {
+        this.service = osv3().workflow().actionDefinitions();
+    }
+
+    @Test
+    public void listActionDefinitions() throws Exception {
+        respondWith(JSON_ACTION_DEFS);
+        
+        List<? extends ActionDefinition> actionDefs = service.list();
+
+        assertEquals(actionDefs.size(), 2);
+
+        // Check first action definition.
+        ActionDefinition actionDef = actionDefs.get(0);
+
+        assertNotNull(actionDef);
+        assertIsUUID(actionDef.getId());
+        assertNotEmptyString(actionDef.getProjectId());
+        assertEquals(actionDef.getName(), "concat");
+        assertNotNull(actionDef.getTags());
+        assertEquals(actionDef.getTags().get(0), "test");
+        assertEquals(actionDef.getTags().get(1), "custom");
+        assertNotNull(actionDef.getCreatedAt());
+        assertNull(actionDef.getUpdatedAt());
+        assertNotEmptyString(actionDef.getDefinition());
+        assertTrue(actionDef.getDefinition().contains("base: std.echo"));
+        assertEquals(actionDef.getInput(), "s1, s2");
+
+        // Check second action definition.
+        actionDef = actionDefs.get(1);
+
+        assertNotNull(actionDef);
+        assertIsUUID(actionDef.getId());
+        assertNotEmptyString(actionDef.getProjectId());
+        assertEquals(actionDef.getName(), "concat_twice");
+        assertNotNull(actionDef.getTags());
+        assertEquals(actionDef.getTags().size(), 0);
+        assertNotNull(actionDef.getCreatedAt());
+        assertNull(actionDef.getUpdatedAt());
+        assertNotEmptyString(actionDef.getDefinition());
+        assertTrue(actionDef.getDefinition().contains("<% $ %> and <% $ %>"));
+        assertEquals(actionDef.getInput(), "s1, s2");
+    }
+
+    @Test
+    public void getActionDefinition() throws Exception {
+        respondWith(JSON_ACTION_DEF);
+
+        ActionDefinition actionDef = service.get("eecf6cad-65af-4a11-9e6f-692b23ffac08");
+
+        assertNotNull(actionDef);
+        assertIsUUID(actionDef.getId());
+        assertNotEmptyString(actionDef.getProjectId());
+        assertEquals(actionDef.getName(), "concat");
+        assertNotNull(actionDef.getTags());
+        assertEquals(actionDef.getTags().get(0), "test");
+        assertEquals(actionDef.getTags().get(1), "custom");
+        assertNotNull(actionDef.getCreatedAt());
+        assertNull(actionDef.getUpdatedAt());
+        assertNotEmptyString(actionDef.getDefinition());
+        assertTrue(actionDef.getDefinition().contains("base: std.echo"));
+        assertEquals(actionDef.getInput(), "s1, s2");
+    }
+
+    @Test
+    public void createActionDefinition() throws Exception {
+        respondWith(JSON_ACTION_DEF_CREATE);
+
+        List<? extends ActionDefinition> actionDefs = service.create(
+                getClass().getResourceAsStream(NEW_ACTION),
+                Scope.PRIVATE
+        );
+
+        assertEquals(actionDefs.size(), 1);
+
+        ActionDefinition actionDef = actionDefs.get(0);
+
+        assertNotNull(actionDef);
+        assertIsUUID(actionDef.getId());
+        assertNotEmptyString(actionDef.getProjectId());
+        assertEquals(actionDef.getName(), "concat");
+        assertNotNull(actionDef.getTags());
+        assertEquals(actionDef.getTags().get(0), "test");
+        assertEquals(actionDef.getTags().get(1), "custom");
+        assertNotNull(actionDef.getCreatedAt());
+        assertNull(actionDef.getUpdatedAt());
+        assertNotEmptyString(actionDef.getDefinition());
+        assertTrue(actionDef.getDefinition().contains("base: std.echo"));
+        assertEquals(actionDef.getInput(), "s1, s2");
+    }
+
+    @Test
+    public void deleteActionDefinition() throws Exception {
+        respondWith(204); // No content.
+
+        HttpResponse resp = service.delete("concat");
+
+        assertEquals(resp.getStatus(), 204);
+    }
+}

core/src/main/java/org/openstack4j/openstack/workflow/internal/ActionDefinitionServiceImpl.java
@@ -1,0 +1,51 @@
+package org.openstack4j.openstack.workflow.internal;
+
+import org.openstack4j.api.workflow.ActionDefinitionService;
+import org.openstack4j.core.transport.HttpResponse;
+import org.openstack4j.model.common.payloads.InputStreamPayload;
+import org.openstack4j.model.workflow.ActionDefinition;
+import org.openstack4j.model.workflow.Scope;
+import org.openstack4j.model.workflow.WorkflowDefinition;
+import org.openstack4j.openstack.workflow.domain.MistralActionDefinition;
+import org.openstack4j.openstack.workflow.domain.MistralActionDefinition.MistralActionDefinitions;
+
+import java.io.InputStream;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Action definition service implementation.
+ *
+ * @author Renat Akhmerov
+ */
+public class ActionDefinitionServiceImpl extends BaseMistralService implements ActionDefinitionService {
+
+    @Override
+    public List<? extends ActionDefinition> list() {
+        return get(MistralActionDefinitions.class, uri("/actions")).execute().getList();
+    }
+
+    @Override
+    public List<? extends ActionDefinition> create(InputStream wfText, Scope scope) {
+        checkNotNull(wfText);
+        checkNotNull(scope);
+
+        Invocation<MistralActionDefinitions> invocation = post(
+                MistralActionDefinitions.class,
+                uri("/actions?scope=%s", scope.toString().toLowerCase())
+        );
+
+        return invocation.entity(new InputStreamPayload(wfText)).execute().getList();
+    }
+
+    @Override
+    public ActionDefinition get(String identifier) {
+        return get(MistralActionDefinition.class, uri("/actions/%s", identifier)).execute();
+    }
+
+    @Override
+    public HttpResponse delete(String identifier) {
+        return deleteWithResponse(uri("/actions/%s", identifier)).executeWithResponse();
+    }
+}

ContainX/openstack4j

core-test/src/main/java/org/openstack4j/api/workflow/ActionDefinitionTest.java
@@ -1,0 +1,129 @@
+package org.openstack4j.api.workflow;
+
+import org.openstack4j.model.common.ActionResponse;
+import org.openstack4j.model.workflow.ActionDefinition;
+import org.openstack4j.model.workflow.Scope;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+/**
+ * Test cases for {@link ActionDefinitionService}.
+ * 
+ * @author Renat Akhmerov
+ */
+@Test(suiteName="ActionDefinitions")
+public class ActionDefinitionTest extends WorkflowBaseTest {
+
+    private static final String JSON_ACTION_DEF = "/workflow/action_def.json";
+    private static final String JSON_ACTION_DEFS = "/workflow/action_defs.json";
+    private static final String NEW_ACTION = "/workflow/new_action.yaml";
+    private static final String JSON_ACTION_DEF_CREATE = "/workflow/action_def_create.json";
+
+    private ActionDefinitionService service;
+
+    @BeforeTest
+    public void setUp() {
+        this.service = osv3().workflow().actionDefinitions();
+    }
+
+    @Test
+    public void listActionDefinitions() throws Exception {
+        respondWith(JSON_ACTION_DEFS);
+        
+        List<? extends ActionDefinition> actionDefs = service.list();
+
+        assertEquals(actionDefs.size(), 2);
+
+        // Check first action definition.
+        ActionDefinition actionDef = actionDefs.get(0);
+
+        assertNotNull(actionDef);
+        assertIsUUID(actionDef.getId());
+        assertNotEmptyString(actionDef.getProjectId());
+        assertEquals(actionDef.getName(), "concat");
+        assertNotNull(actionDef.getTags());
+        assertEquals(actionDef.getTags().get(0), "test");
+        assertEquals(actionDef.getTags().get(1), "custom");
+        assertNotNull(actionDef.getCreatedAt());
+        assertNull(actionDef.getUpdatedAt());
+        assertNotEmptyString(actionDef.getDefinition());
+        assertTrue(actionDef.getDefinition().contains("base: std.echo"));
+        assertEquals(actionDef.getInput(), "s1, s2");
+
+        // Check second action definition.
+        actionDef = actionDefs.get(1);
+
+        assertNotNull(actionDef);
+        assertIsUUID(actionDef.getId());
+        assertNotEmptyString(actionDef.getProjectId());
+        assertEquals(actionDef.getName(), "concat_twice");
+        assertNotNull(actionDef.getTags());
+        assertEquals(actionDef.getTags().size(), 0);
+        assertNotNull(actionDef.getCreatedAt());
+        assertNull(actionDef.getUpdatedAt());
+        assertNotEmptyString(actionDef.getDefinition());
+        assertTrue(actionDef.getDefinition().contains("<% $ %> and <% $ %>"));
+        assertEquals(actionDef.getInput(), "s1, s2");
+    }
+
+    @Test
+    public void getActionDefinition() throws Exception {
+        respondWith(JSON_ACTION_DEF);
+
+        ActionDefinition actionDef = service.get("eecf6cad-65af-4a11-9e6f-692b23ffac08");
+
+        assertNotNull(actionDef);
+        assertIsUUID(actionDef.getId());
+        assertNotEmptyString(actionDef.getProjectId());
+        assertEquals(actionDef.getName(), "concat");
+        assertNotNull(actionDef.getTags());
+        assertEquals(actionDef.getTags().get(0), "test");
+        assertEquals(actionDef.getTags().get(1), "custom");
+        assertNotNull(actionDef.getCreatedAt());
+        assertNull(actionDef.getUpdatedAt());
+        assertNotEmptyString(actionDef.getDefinition());
+        assertTrue(actionDef.getDefinition().contains("base: std.echo"));
+        assertEquals(actionDef.getInput(), "s1, s2");
+    }
+
+    @Test
+    public void createActionDefinition() throws Exception {
+        respondWith(JSON_ACTION_DEF_CREATE);
+
+        List<? extends ActionDefinition> actionDefs = service.create(
+                getClass().getResourceAsStream(NEW_ACTION),
+                Scope.PRIVATE
+        );
+
+        assertEquals(actionDefs.size(), 1);
+
+        ActionDefinition actionDef = actionDefs.get(0);
+
+        assertNotNull(actionDef);
+        assertIsUUID(actionDef.getId());
+        assertNotEmptyString(actionDef.getProjectId());
+        assertEquals(actionDef.getName(), "concat");
+        assertNotNull(actionDef.getTags());
+        assertEquals(actionDef.getTags().get(0), "test");
+        assertEquals(actionDef.getTags().get(1), "custom");
+        assertNotNull(actionDef.getCreatedAt());
+        assertNull(actionDef.getUpdatedAt());
+        assertNotEmptyString(actionDef.getDefinition());
+        assertTrue(actionDef.getDefinition().contains("base: std.echo"));
+        assertEquals(actionDef.getInput(), "s1, s2");
+    }
+
+    @Test
+    public void deleteActionDefinition() throws Exception {
+        respondWith(204); // No content.
+
+        ActionResponse resp = service.delete("concat");
+
+        // TODO(rakhmerov): Change to 204 once ActionResponse can return other 2xx codes.
+        assertEquals(resp.getCode(), 200);
+    }
+}

core/src/main/java/org/openstack4j/openstack/workflow/internal/ActionDefinitionServiceImpl.java
@@ -1,0 +1,50 @@
+package org.openstack4j.openstack.workflow.internal;
+
+import org.openstack4j.api.workflow.ActionDefinitionService;
+import org.openstack4j.model.common.ActionResponse;
+import org.openstack4j.model.common.payloads.InputStreamPayload;
+import org.openstack4j.model.workflow.ActionDefinition;
+import org.openstack4j.model.workflow.Scope;
+import org.openstack4j.openstack.workflow.domain.MistralActionDefinition;
+import org.openstack4j.openstack.workflow.domain.MistralActionDefinition.MistralActionDefinitions;
+
+import java.io.InputStream;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Action definition service implementation.
+ *
+ * @author Renat Akhmerov
+ */
+public class ActionDefinitionServiceImpl extends BaseMistralService implements ActionDefinitionService {
+
+    @Override
+    public List<? extends ActionDefinition> list() {
+        return get(MistralActionDefinitions.class, uri("/actions")).execute().getList();
+    }
+
+    @Override
+    public List<? extends ActionDefinition> create(InputStream wfText, Scope scope) {
+        checkNotNull(wfText);
+        checkNotNull(scope);
+
+        Invocation<MistralActionDefinitions> invocation = post(
+                MistralActionDefinitions.class,
+                uri("/actions?scope=%s", scope.toString().toLowerCase())
+        );
+
+        return invocation.entity(new InputStreamPayload(wfText)).execute().getList();
+    }
+
+    @Override
+    public ActionDefinition get(String identifier) {
+        return get(MistralActionDefinition.class, uri("/actions/%s", identifier)).execute();
+    }
+
+    @Override
+    public ActionResponse delete(String identifier) {
+        return deleteWithResponse(uri("/actions/%s", identifier)).execute();
+    }
+}