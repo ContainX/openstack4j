d37d474d643d316cdbb07a318c08c29592bb7e7f
Woo Cubic <iampurse@vip.qq.com>
2017-06-10 23:05:05 +0800

    fix copy right

--- core/src/main/java/org/openstack4j/model/network/builder/AgentBuilder.java
+++ core/src/main/java/org/openstack4j/model/network/builder/AgentBuilder.java
@@ -3,17 +3,21 @@ package org.openstack4j.model.network.builder;
 import org.openstack4j.common.Buildable.Builder;
 import org.openstack4j.model.network.Agent;
 
-/**
- * <p>
- * <b>Copyright:</b> Copyright (c) 2016
- * </p>
- * <p>
- * <b>Company:</b> Ericsson
- * </p>
- *
- * @author emagnbr 2016- initial version
- *
- */
+/*******************************************************************************
+ * 	Copyright 2016 ContainX and OpenStack4j                                          
+ * 	                                                                                 
+ * 	Licensed under the Apache License, Version 2.0 (the "License"); you may not      
+ * 	use this file except in compliance with the License. You may obtain a copy of    
+ * 	the License at                                                                   
+ * 	                                                                                 
+ * 	    http://www.apache.org/licenses/LICENSE-2.0                                   
+ * 	                                                                                 
+ * 	Unless required by applicable law or agreed to in writing, software              
+ * 	distributed under the License is distributed on an "AS IS" BASIS, WITHOUT        
+ * 	WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the         
+ * 	License for the specific language governing permissions and limitations under    
+ * 	the License.                                                                     
+ *******************************************************************************/
 public interface AgentBuilder extends Builder<AgentBuilder, Agent> {
 
     /**
