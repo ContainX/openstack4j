a26d4c1705a55b61d7ec350792eae242da7eb7dc
Woo Cubic <iampurse@vip.qq.com>
2017-07-04 12:06:53 +0800

    CloudEye add metric data API issue #8

--- core/src/main/java/org/openstack4j/openstack/cloudeye/domain/CloudEyeMetric.java
+++ core/src/main/java/org/openstack4j/openstack/cloudeye/domain/CloudEyeMetric.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.openstack4j.model.cloudeye.Metric;
 import org.openstack4j.openstack.common.ListResult;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -24,13 +25,13 @@ public class CloudEyeMetric implements Metric {
 
 	private static final long serialVersionUID = -4166055531397254389L;
 
-	String id;
 	String namespace;
 	List<CloudEyeMetricDemension> dimensions;
 	@JsonProperty("metric_name")
 	String metricName;
 	String unit;
 
+	@JsonIgnore
 	public String getId() {
 		StringBuffer sb = new StringBuffer();
 		sb.append(namespace).append(".").append(metricName);
