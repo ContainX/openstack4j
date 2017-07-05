9abcbd8e4edbc1070d9c7f33626d1b2c0f18b95e
Woo Cubic <iampurse@vip.qq.com>
2017-07-05 21:09:03 +0800

    auto_scaling group available zone mapping issue #11

--- core/src/main/java/org/openstack4j/openstack/scaling/domain/ASAutoScalingGroup.java
+++ core/src/main/java/org/openstack4j/openstack/scaling/domain/ASAutoScalingGroup.java
@@ -81,7 +81,7 @@ public class ASAutoScalingGroup implements ScalingGroup {
 	@JsonProperty("lb_listener_id")
 	String lbListenerId;
 
-	@JsonProperty("availability_zones")
+	@JsonProperty("available_zones")
 	List<String> availabilityZones;
 
 	@JsonProperty("networks")
