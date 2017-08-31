13df2ba074bc8902f39ecc09365fe75f5fc75d9f
Rafael Bertoli <rafael.bertoli@criarenet.com>
2017-08-31 21:47:26 +0000

    attach and detach network to agent dhcp based on neutron spec https://specs.openstack.org/openstack/neutron-specs/specs/api/agent_schedulers.html#list-networks-hosted-by-a-dhcp-agent I can't do attach test because the request return null and break test with resteasy provider

--- core/src/main/java/org/openstack4j/openstack/networking/internal/ext/AgentServiceImpl.java
+++ core/src/main/java/org/openstack4j/openstack/networking/internal/ext/AgentServiceImpl.java
@@ -1,12 +1,11 @@
 package org.openstack4j.openstack.networking.internal.ext;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-
 import java.util.List;
-
 import org.openstack4j.api.networking.ext.AgentService;
 import org.openstack4j.core.transport.ExecutionOptions;
 import org.openstack4j.core.transport.propagation.PropagateOnStatus;
+import org.openstack4j.model.common.ActionResponse;
 import org.openstack4j.model.network.Agent;
 import org.openstack4j.openstack.networking.domain.NeutronAgent;
 import org.openstack4j.openstack.networking.domain.NeutronAgent.Agents;
@@ -36,5 +35,18 @@ public class AgentServiceImpl extends BaseNetworkingServices implements AgentSer
         String json = String.format("{\"%s\": { \"%s\": \"%b\"}}", "agent", "admin_state_up", state);
         return put(NeutronAgent.class, uri("/agents/%s", agentId)).json(json).execute(
                 ExecutionOptions.<NeutronAgent> create(PropagateOnStatus.on(404)));
+    }
+
+    @Override
+    public ActionResponse attachNetworkToDhcpAgent(String agentId, String networkId) {
+        checkNotNull(agentId);
+        String json = String.format("{\"%s\": \"%s\"}", "network_id", networkId);
+        return postWithResponse(uri("/agents/%s/dhcp-networks", agentId)).json(json).execute(ExecutionOptions.<ActionResponse> create(PropagateOnStatus.on(404)));
+    }
+    
+    @Override
+    public ActionResponse detachNetworkToDhcpAgent(String agentId, String networkId) {
+        checkNotNull(agentId);
+        return deleteWithResponse(uri("/agents/%s/dhcp-networks/%s", agentId, networkId)).execute(ExecutionOptions.<ActionResponse> create(PropagateOnStatus.on(404)));
     }
 }
