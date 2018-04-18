366911e70702e2d81bdde3c84e52ccb54708728c
Cherry522 <522581280@qq.com>
2018-04-18 17:58:07 +0800

    add gnocchi

---
+++ core/src/main/java/org/openstack4j/openstack/gnocchi/builder/GnocchiBuilders.java
@@ -0,0 +1,16 @@
+package org.openstack4j.openstack.gnocchi.builder;
+
+import org.openstack4j.model.gnocchi.builder.ResourceBuilder;
+
+/**
+ * The gnocchi builders
+ * @author zhangliang
+ */
+public class GnocchiBuilders implements org.openstack4j.model.gnocchi.builder.GnocchiBuilders {
+
+    @Override
+    public ResourceBuilder resource() {
+        return null;
+    }
+
+}
