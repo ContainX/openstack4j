e986529e4d96b08e44e5f9fd119ee45a6abeac83
Cherry522 <522581280@qq.com>
2018-05-03 18:02:59 +0800

    update ironic add node management methods

--- core/src/main/java/org/openstack4j/openstack/bareMetal/domain/BareMetalNodeManagement.java
+++ core/src/main/java/org/openstack4j/openstack/bareMetal/domain/BareMetalNodeManagement.java
@@ -14,6 +14,15 @@ public class BareMetalNodeManagement implements NodeManagement {
     @JsonProperty("target")
     private String target;
 
+    @JsonProperty("boot_device")
+    private String bootDevice;
+
+    @JsonProperty("persistent")
+    private Boolean persistent;
+
+    @JsonProperty("enabled")
+    private Boolean enabled;
+
     @Override
     public NodeManagementBuilder toBuilder() {
         return new NodeManagementConcreteBuilder(this);
@@ -38,6 +47,24 @@ public class BareMetalNodeManagement implements NodeManagement {
         @Override
         public NodeManagementBuilder target(String target) {
             m.target = target;
+            return this;
+        }
+
+        @Override
+        public NodeManagementBuilder bootDevice(String bootDevice) {
+            m.bootDevice = bootDevice;
+            return this;
+        }
+
+        @Override
+        public NodeManagementBuilder persistent(Boolean persistent) {
+            m.persistent = persistent;
+            return this;
+        }
+
+        @Override
+        public NodeManagementBuilder enabled(Boolean enabled) {
+            m.enabled = enabled;
             return this;
         }
 
