c62ef61b8d6a3b0923c5cc28aaed50a5b185dc87
Luigi De Masi <luigidemasi@gmail.com>
2019-06-14 16:16:25 +0200

    param 'external_fixed_ps' of external_gateway_info is missing #854
    https://github.com/ContainX/openstack4j/issues/854

--- core/src/main/java/org/openstack4j/model/network/ExternalGateway.java
+++ core/src/main/java/org/openstack4j/model/network/ExternalGateway.java
@@ -1,6 +1,9 @@
 package org.openstack4j.model.network;
 
 import org.openstack4j.model.ModelEntity;
+import org.openstack4j.openstack.networking.domain.NeutronEternalFixedIps;
+
+import java.util.List;
 
 /**
  * Configurable external gateway modes extension model.  By default, when a gateway is attached to a router using the Neutron L3 extension,
@@ -16,15 +19,17 @@ public interface ExternalGateway extends ModelEntity {
 	 * @return the network identifier
 	 */
 	String getNetworkId();
-	
+
 	/**
-	 * SNAT can be enabled or disabled at any time on a Neutron router regardless of the current status of floating IPs. 
-	 * Floating IPs will continue working even when SNAT is disabled. 
-	 * 
+	 * SNAT can be enabled or disabled at any time on a Neutron router regardless of the current status of floating IPs.
+	 * Floating IPs will continue working even when SNAT is disabled.
+	 * <p>
 	 * NOTE: The default setting is True to ensure backward compatibility for plugins supporting this extension.
-	 * 
+	 *
 	 * @return true if SNAT is enabled
 	 */
 	boolean isEnableSnat();
-	
+
+
+	List<? extends ExternalFixedIps> getExternalFixedIps();
 }
